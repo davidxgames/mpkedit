b1911a0ebd94046684c22f41286b94b985e0e9f9
davidxgames <100103700+davidxgames@users.noreply.github.com>
2024-09-30 15:10:06 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function: changeTheme(i)
   Change theme to specified theme
 */
 const changeTheme = function(id) {
-    if(id) document.body.className = "Dark theme"
+    if(id) document.body.className = "theme-dark"
     // not the best solution, but it works.
     else if(document.body.className) document.body.className = "";
 };
